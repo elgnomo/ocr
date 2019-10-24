0034750bccb1bbf1a4267d84b190d18e81cef664
Alberto Luebbert <alberto.luebbert@fanbase.mx>
2019-10-24 16:53:11 -0500

    Index modificado

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,15 +1,14 @@
 var imagenFrontal = Ti.UI.createImageView({
     width: "100%",
     height: "100%",
-    autorotate: true,
+    autorotate: true
 })
 
-
-$.topCredencial.addEventListener('click', function(e) {
+$.topCredencial.addEventListener('click', function (e) {
     if (Ti.Media.hasCameraPermissions()) {
         openCameraf();
     } else {
-        Ti.Media.requestCameraPermissions(function(e) {
+        Ti.Media.requestCameraPermissions(function (e) {
             if (e.success) {
                 openCameraf();
             } else {
@@ -20,48 +19,96 @@ $.topCredencial.addEventListener('click', function(e) {
 });
 
 var image;
+var appPhoto;
+var form_url = "https://www.ideashappy.com/ahorra/new.php";
 
 function openCameraf() {
+    Ti.Media.openPhotoGallery({
+        mediaTypes: [Ti.Media.MEDIA_TYPE_PHOTO],
+        success: function (e) {
+            //btnPhoto.value = e.media;
+            //anImageView.image = e.media;
+            //btnPhoto.title = '[ Photo Selected ]';
+            var c = Titanium.Network.createHTTPClient({
+                onload: function (e) {
+                    console.log('por aca');
+
+                    Ti.API.info('this.responseText' + this.responseText);
+                    //json = JSON.stringify(this.responseText);
+                    alert(this.responseText);
+                },
+                onerror: function (e) {
+                    console.log('pro aqui');
+                    alert(JSON.stringify(e));
+                }
+            });
+            c.open('POST', form_url);
+            Ti.API.info('form_url ' + encodeURI(form_url));
+            c.setRequestHeader('Content-Type', 'multipart/form-data');
+            c.send({
+                userid: 81,
+                image: e.media
+            });
+        },
+        cancel: function () {
+            //btnPhoto.value = null;
+            ///btnPhoto.title = 'Select Photo...';
+            console.log('cancel');
+        },
+        error: function (err) {
+
+            //btnPhoto.value = null;
+            //btnPhoto.title = 'Select Photo...';
+            console.log('error');
+            Ti.API.error(err);
+        }
+    });
+}
+
+function resultado() {
+    console.log('resultado');
+}
+
+function openCameraf2() {
+
     $.frontal.add(imagenFrontal);
     var dialog = Titanium.UI.createOptionDialog({
         title: 'Seleccion el Tipo de Imagen',
         options: ['Camara', 'Galeria de Fotos', 'Cancelar'],
         cancel: 2
     });
-    dialog.addEventListener('click', function(e) {
+    dialog.addEventListener('click', function (e) {
         if (e.index == 0) {
             Titanium.Media.showCamera({
-                success: function(e) {
+                success: function (e) {
                     image = e.media;
                     if (e.mediaType == Ti.Media.MEDIA_TYPE_PHOTO) {
                         imagenFrontal.image = image;
-						var url = 'https://www.ideashappy.com/ahorra/otro.php';
-						//var url = "http://www.mark-43.net/mark43-service/v1/api-drive/subir-imagen"
+                        var url = 'https://www.ideashappy.com/ahorra/nuevo.php';
                         var xhr = Titanium.Network.createHTTPClient({
-                            onload: function(e) {
-								Ti.API.info('Info received from the uploading: ' + this.responseText);
+                            onload: function (e) {
+                                Ti.API.info('2. Info received from the uploading: ' + this.responseText);
                                 // var json = JSON.parse(this.responseText);
                                 // console.log("JSON = ", json);
                             },
-                            onerror: function(e) {
+                            onerror: function (e) {
                                 console.log("Error", e);
                             },
                             timeout: 200000
-						});
+                        });
 
                         var params = {
-                            myfile: e.media,
-						};
-						var alerta = JSON.stringify(e.media);
-						alert(alerta);
+                            media: e.media,
+                        };
+                        ///var alerta = JSON.stringify(e.media);
+                        //alert(alerta);
                         xhr.open("POST", url);
                         xhr.setRequestHeader('Content-Type', 'multipart/form-data');
-						xhr.send(alerta);
-						//console.log("Paramatros =", params);
+                        xhr.send(params);
                     }
                 },
-                cancel: function() {},
-                error: function(error) {
+                cancel: function () { },
+                error: function (error) {
                     var a = Titanium.UI.createAlertDialog({
                         title: 'Camera'
                     });
@@ -77,39 +124,39 @@ function openCameraf() {
             });
         } else if (e.index == 1) {
             Titanium.Media.openPhotoGallery({
-                success: function(e) {
+                success: function (e) {
                     image = e.media;
                     if (e.mediaType == Ti.Media.MEDIA_TYPE_PHOTO) {
                         imagenFrontal.image = image;
-                        var url = 'https://www.ideashappy.com/ahorra/otro.php';
-						//var url = "http://www.mark-43.net/mark43-service/v1/api-drive/subir-imagen"
+                        var url = 'https://www.ideashappy.com/ahorra/nuevo.php';
+                        //var url = "http://www.mark-43.net/mark43-service/v1/api-drive/subir-imagen"
                         var xhr = Titanium.Network.createHTTPClient({
-                            onload: function(e) {
-								console.log('url ', url);
-								console.log('Info received from the uploading: ' + this.responseText);
-								console.log("parametro = ",alerta)
+                            onload: function (e) {
+                                console.log('url ', url);
+                                console.log('1 Info received from the uploading: ' + this.responseText);
+                                //console.log("parametro = ",alerta)
                                 // var json = JSON.parse(this.responseText);
                                 // console.log("JSON = ", json);
                             },
-                            onerror: function(e) {
+                            onerror: function (e) {
                                 console.log("Error", e);
                             },
                             timeout: 200000
-						});
+                        });
 
                         var params = {
-                            myfile: e.media,
-						};
-						var alerta = JSON.stringify(params);
-						//alert(alerta);
+                            media: e.media,
+                        };
+                        var alerta = JSON.stringify(params);
+                        //alert(alerta);
                         xhr.open("POST", url);
-                        xhr.setRequestHeader('Content-Type', 'multipart/form-data');
-						xhr.send(alerta);
-						console.log("ALERTA = ",alerta)
-						
+                        //xhr.setRequestHeader('Content-Type', 'multipart/form-data');
+                        xhr.send(params);
+                        //console.log("ALERTA = ",alerta)
+
                     }
                 },
-                cancel: function() {
+                cancel: function () {
                     //user cancelled the action fron within
                     //the photo gallery
                 }
@@ -120,39 +167,6 @@ function openCameraf() {
         }
     });
     dialog.show();
-}
-
-
-var imagenReverso = Ti.UI.createImageView({
-    width: "100%",
-    height: "100%",
-    autorotate: true,
-});
-
-$.bottomCredencial.addEventListener('click', function(e) {
-    if (Ti.Media.hasCameraPermissions()) {
-        openCamerar();
-    } else {
-        Ti.Media.requestCameraPermissions(function(e) {
-            if (e.success) {
-                openCamerar();
-            } else {
-                alert('You denied permission');
-            }
-        });
-    }
-});
-
-
-function openCamerar() {
-    $.bottomCredencial.add(imagenReverso);
-    Ti.Media.showCamera({
-        success: function(e) {
-            // display image information:
-            console.log(e.media);
-            imagenReverso.image = e.media;
-        }
-    })
 }
 
 
